b68c8a45e8aba49d68bb856302c0706fc3bdd740
Bjørn Winther <bwinth18@student.aau.dk>
2022-05-13 02:03:06 +0200

    Small adjustments to miniprefabs

--- Assets/Resources/miniPrefab.cs
+++ Assets/Resources/miniPrefab.cs
@@ -18,6 +18,8 @@ public class miniPrefab : MonoBehaviour
     private Vector3 targetPositionTest;
     private Vector3 initialScale;
     private Quaternion initialRotation;
+    Material originalMat;
+    Material mat;
 
     void Start()
     {
@@ -26,6 +28,27 @@ public class miniPrefab : MonoBehaviour
         initialScale = transform.localScale;
         scaleChange = new Vector3(0.01f,0.01f,0.01f);
         initialRotation = transform.rotation;
+        /*
+        if (gameObject.CompareTag("MiniMace"))
+        {
+            originalMat = gameObject.GetComponentInChildren<MeshRenderer>().material;
+        }else if (gameObject.CompareTag("MiniSpears"))
+        {
+            originalMat = gameObject.GetComponentInChildren<MeshRenderer>().material;
+        }
+        else if (gameObject.CompareTag("MiniFist"))
+        {
+            originalMat = gameObject.GetComponent<MeshRenderer>().material;
+        }
+        else if (gameObject.CompareTag("MiniArrows"))
+        {
+            originalMat = gameObject.GetComponentInChildren<MeshRenderer>().material;
+        }
+        //originalMat = gameObject.GetComponent<MeshRenderer>().material;
+        if(originalMat != null) { return; }
+        mat = new Material(originalMat);
+        gameObject.GetComponent<MeshRenderer>().material = mat;
+        */
     }
 
     // Update is called once per frame
@@ -45,6 +68,7 @@ public class miniPrefab : MonoBehaviour
                 finalPosition = targetTransform.position + new Vector3(0, 4.5f, 0);
                 transform.localScale = Vector3.Lerp(initialScale, new Vector3(0.5f, 0.5f, 0.5f), elapsedTime / 2);
                 transform.position = Vector3.Lerp(initialPosition, finalPosition, elapsedTime / 2);
+                transform.rotation = Quaternion.Lerp(initialRotation, Quaternion.Euler(new Vector3(0, 360, 0)), elapsedTime / 2);
 
                 //finalPosition = targetPositionTest + new Vector3(0, 3.5f, 0);
                 //transform.localScale += scaleChange;//* Time.deltaTime;
@@ -53,7 +77,7 @@ public class miniPrefab : MonoBehaviour
             else if (gameObject.CompareTag("MiniSpears"))
             {
                 finalPosition = targetTransform.position + new Vector3(0,-0.3f,0);
-                transform.localScale = Vector3.Lerp(initialScale, new Vector3(0.65f,0.65f,0.65f), elapsedTime / 2);
+                transform.localScale = Vector3.Lerp(initialScale, new Vector3(0.7f,0.7f,0.7f), elapsedTime / 2);
                 transform.position = Vector3.Lerp(initialPosition, finalPosition, elapsedTime / 2);
                 transform.rotation = Quaternion.Lerp(initialRotation,Quaternion.Euler(new Vector3(0,360,0)), elapsedTime / 2);
                 //finalPosition = targetPositionTest + new Vector3(0, 3.5f, 0);
@@ -62,7 +86,7 @@ public class miniPrefab : MonoBehaviour
             }
             else if (gameObject.CompareTag("MiniFist"))
             {
-                finalPosition = targetTransform.position + new Vector3(0, 4.5f, 0);
+                finalPosition = targetTransform.position + new Vector3(0, 4f, 0);
                 transform.localScale = Vector3.Lerp(initialScale, new Vector3(0.1f, 0.1f, 0.1f), elapsedTime / 2);
                 transform.position = Vector3.Lerp(initialPosition, finalPosition, elapsedTime / 2);
                 transform.rotation = Quaternion.Lerp(initialRotation, Quaternion.Euler(new Vector3(0, 0, 270)), elapsedTime / 2);
@@ -71,26 +95,18 @@ public class miniPrefab : MonoBehaviour
             }
             else if (gameObject.CompareTag("MiniArrows"))
             {
-                finalPosition = targetTransform.position + new Vector3(0, 0, 0);
+                finalPosition = targetTransform.position + new Vector3(0, 0.18f, 0);
                 transform.localScale = Vector3.Lerp(initialScale, new Vector3(0.55f, 0.55f, 0.55f), elapsedTime / 2);
                 transform.position = Vector3.Lerp(initialPosition, finalPosition, elapsedTime / 2);
+                transform.rotation = Quaternion.Lerp(initialRotation, Quaternion.Euler(new Vector3(0, 270, 0)), elapsedTime / 2);
             }
-            //float elapsedTime = Time.time - spawnTime;
-
             /*
-            if (transform.localScale.x < 1.5f)
-            {
-                transform.localScale += scaleChange;//* Time.deltaTime;
-                transform.RotateAround(transform.position, Vector3.up, 80 * Time.deltaTime); // rotate around self with 5 degrees per sec.
-            }*/
+            float sineAlpha = Mathf.Sin(Time.time * 4);
+            Color color = new Color(originalMat.color.r, originalMat.color.g, originalMat.color.b, sineAlpha);
 
-            //float sineAlpha = Mathf.Sin(Time.time * 4);
-            //Color color = new Color(originalMat.color.r, originalMat.color.g, originalMat.color.b, sineAlpha);
-            //transform.position += new Vector3(0.1f, 0.1f, 0.1f);
-
-            //mat.color = color;
-
-            if (elapsedTime > 2.1f) // set to 2.5 after
+            mat.color = color;
+            */
+            if (elapsedTime > 2.3f) // set to 2.5 after
             {
                 isTargetPosReached = true;
                 playerObj.GetComponent<PlayerBehaviour>().miniPrefabOnLoc = true;
